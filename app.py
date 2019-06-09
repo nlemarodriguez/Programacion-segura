5c587ea913579e45cdb421bd8b789b079a6ad456
Nicolas <nicolaslema7@gmail.com>
2019-06-09 12:38:03 -0500

    Ajustes de forma y al gitignore

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from database import Database
-import pymysql
 
 
 def create_app():
-  app = Flask(__name__)
-  Bootstrap(app)
-  return app
+    app = Flask(__name__)
+    Bootstrap(app)
+    return app
+
 
 app = create_app()
+
 
 @app.route('/')
 def login():
     return render_template('login.html')
 
+
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
+
 
 @app.route('/users')
 def users():
@@ -29,5 +32,6 @@ def users():
     return str(res)
     #return render_template('employees.html', result=res, content_type='application/json')
 
+
 if __name__ == '__main__':
-    app.run(port = 3000, debug = True)
+    app.run(port=3000, debug=True)
--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-from flask import Flask
 import pymysql
+
 
 class Database:
     def __init__(self):
