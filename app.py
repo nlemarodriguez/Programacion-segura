9b82ba1bf9fcb2714b1c288d436f5d62ca2dbdbd
Daury <daurylecca16@gmail.com>
2019-06-10 23:54:45 -0500

    Se ajusta método de registro

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, url_for, jsonify, flash, redi
 from flask_bootstrap import Bootstrap
 from database import Database
 
-#UPLOAD_FOLDER = '/static/imagen_perfil'
+URL_FOLDER = '/static/imagen_perfil/'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static', 'imagen_perfil')
@@ -82,34 +82,32 @@ def allowed_file(filename):
 @app.route('/registrar_usuario', methods=['POST'])
 def reister():
     if request.method == 'POST':
-        #if 'file' not in request.files:
-        # latestfile = request.files['customlogo']
-        #         # full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'logo.png')
-        #         # latestfile.save(full_filename)
-
-
-        file = request.files['files[]']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file and allowed_file(file.filename):
-            filename = file.filename
-            flash('file {} saved'.format(file.filename))
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            #file.save(app.config['UPLOAD_FOLDER'] + "/" + filename)
-
         first_name = request.form['first_name']
         last_name = request.form['last_name']
         email = request.form['email']
         password = request.form['password']
         gender = request.form['gender']
-        photo = '/static/imagen_perfil' + '/' + file.filename
+        file = request.files['files[]']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            photo = URL_FOLDER + file.filename
+        else:
+            if gender == 'F':
+                photo = URL_FOLDER + 'woman-img.png'
+            else:
+                photo = URL_FOLDER + 'man-img.png'
+
         existeCorreo = db.verificar_correo(email)
         if existeCorreo:
-            return str("Usuario ya existe")
+            flash('Usuario ya existe')
+            return render_template('registro.html')
         else:
             db.registrar_post(first_name, last_name, email, password, gender, photo)
-            return str('usuario registrado')
-
+            flash('Usuario registrado con éxito')
+            return redirect(url_for('index'))
 
 @app.route('/delete_commmet/<id>', methods=['POST', 'GET'])
 def delete_commet(id):
